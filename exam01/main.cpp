dd65786d9b2a910a39ca2be62ae389f440de3741
AlvinChuang <alvin870203@gapp.nthu.edu.tw>
2020-04-01 17:38:05 +0800

    finish sample from D7

--- exam01/main.cpp
+++ exam01/main.cpp
@@ -3,6 +3,8 @@
 
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 PwmOut PWM1(D6);
+Serial pc( USBTX, USBRX );
+DigitalIn D7data(D7);
 
 int main()
 {
@@ -12,16 +14,21 @@ int main()
 
     // PWM
     int i; // 10*dutyCycle
+    float data;
     while(1){
         for (i=0; i<=10; i++){
             PWM1.period(0.001); // 1000Hz
             PWM1 = ((float)i/10);
             wait(0.1);
+            data = D7data;
+            pc.printf("%f\r\n", data);
         }
         for (i=9; i>=1; i--){
             PWM1.period(0.001); // 1000Hz
             PWM1 = ((float)i/10);
-            wait(0.1); 
+            wait(0.1);
+            data = D7data;
+            pc.printf("%f\r\n", data);
         }   
     }
 
