b8514abcc6633f0311082215e980330e71075fb9
AlvinChuang <alvin870203@gapp.nthu.edu.tw>
2020-04-01 17:26:55 +0800

    finish sweeping PWM(D6) & picoscope wave measure

--- exam01/main.cpp
+++ exam01/main.cpp
@@ -2,10 +2,31 @@
 #include "uLCD_4DGL.h"
 
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
+PwmOut PWM1(D6);
 
 int main()
 {
-    // basic printf demo = 16 by 18 characters on screen
-    uLCD.printf("\n105033110\n"); //Default Green on black text
+    // uLCD
+    uLCD.printf("\n105033110\n");
     uLCD.filled_rectangle(50, 50, 100, 90, 0x00FF00);
+
+    // PWM
+    int i; // 10*dutyCycle
+    while(1){
+        for (i=0; i<=10; i++){
+            PWM1.period(0.001); // 1000Hz
+            PWM1 = ((float)i/10);
+            wait(0.1);
+        }
+        for (i=9; i>=1; i--){
+            PWM1.period(0.001); // 1000Hz
+            PWM1 = ((float)i/10);
+            wait(0.1); 
+        }   
+    }
+
+    // D7
+
+    
+
 }
